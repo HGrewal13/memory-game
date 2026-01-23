eb39dde8c715994229d4042d6f832084101d529e
Harry <harrysgrewal13@gmail.com>
2026-01-22 19:30:19 -0500

    working api call. updates pokemonList with name, id and sprite url

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const url = "https://pokeapi.co/api/v2/pokemon/?limit=151"; //For options on changing generations, url would need to be a dependency in useEffect
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error("No response");
+        }
+        return response.json();
+      })
+      .then(data => {
+        const results = data.results;
+        
+        results.forEach(result => {
+          const id = result.url.split("/").at(-2);
+          const spriteURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+          setPokemonList(...pokemonList, {name: result.name, id: id, sprite: spriteURL});
+        })
+
+        
+      })
+      .catch(error => {
+        console.error("There was a problem with the fetch operation: ", error);
+      })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Memory Game</h1>
     </>
   )
 }
